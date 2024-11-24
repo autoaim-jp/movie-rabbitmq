9e50c58cc5c1ecb4a16266df03227c4efda3f0b1
autoaim_jp <git@autoaim.jp>
2024-11-24 18:45:19 +0900

    test: curl_post_main.sh

--- app/test/curl_post_main.sh
+++ app/test/curl_post_main.sh
@@ -2,12 +2,19 @@
 
 ls -lat service/movieEngine/src/data/
 
+SCRIPT_DIR_PATH=$(dirname "$0")/
+
+ls ${SCRIPT_DIR_PATH}asset/narration.csv
+ls ${SCRIPT_DIR_PATH}asset/dummy_image1.png
+
+NARRATION_TEXT=$(cat ${SCRIPT_DIR_PATH}asset/narration.csv)
+
 curl -X POST \
   -F "title=test シマリス兄弟とふしぎなショッピングモール。" \
-  -F "narrationCsv=${PWD}/asset/narration.csv" \
-  -F "fileList[]=@${PWD}/asset/dummy_image1.png" \
-  -F "fileList[]=@${PWD}/asset/dummy_image2.png" \
-  -F "fileList[]=@${PWD}/asset/dummy_image3.png" \
-  -F "fileList[]=@${PWD}/asset/dummy_image4.png" \
+  -F "narrationCsv=${NARRATION_TEXT}" \
+  -F "fileList[]=@${SCRIPT_DIR_PATH}asset/dummy_image1.png" \
+  -F "fileList[]=@${SCRIPT_DIR_PATH}asset/dummy_image2.png" \
+  -F "fileList[]=@${SCRIPT_DIR_PATH}asset/dummy_image3.png" \
+  -F "fileList[]=@${SCRIPT_DIR_PATH}asset/dummy_image4.png" \
   http://localhost:25673/api/v1/prompt/register/main
 
